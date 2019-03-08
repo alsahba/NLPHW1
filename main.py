f4cfb7fb71806c2249b92e8594e317c19474b27a
alsahba <alisahinbalikci@gmail.com>
2019-03-08 12:31:25 +0300

    Word Counter Added

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
-print('smt')
-print('denemeGithub')
+def countWords (mappingList, key):
+    if len(mappingList) == 0:
+        newDict = {'key': key,
+                   'count': 1}
+        mappingList.append(newDict)
+
+    else:
+        foundFlag = False
+        for mapping in mappingList:
+            if str(mapping.get('key')) == key:
+                var = int(mapping.get('count')) + 1
+                mapping['count'] = var
+                foundFlag = True
+                break
+
+        if foundFlag == False:
+            newDict = {'key': key,
+                       'count': 1}
+            mappingList.append(newDict)
+
+
+f = open("1.txt", "r")
+
+unigram_word_mapping = []
+
+
+for line in f.readlines():
+    splitted_line = line.split(' ')
+    for word in splitted_line:
+        if word != '\n':
+            countWords(unigram_word_mapping, word.lower())
+
+print(unigram_word_mapping)
+
+
+
+
